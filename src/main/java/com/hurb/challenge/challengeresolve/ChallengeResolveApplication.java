4b38ed6af1bbd4cb898f42542feddbaaac470c29
Gabriel K JR <gabrielkopke668@gmail.com>
2024-03-22 11:13:24 -0300

    raw: inital conf to spring project

---
+++ src/main/java/com/hurb/challenge/challengeresolve/ChallengeResolveApplication.java
@@ -0,0 +1,13 @@
+package com.hurb.challenge.challengeresolve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeResolveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeResolveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hurb/challenge/challengeresolve/ChallengeResolveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hurb.challenge.challengeresolve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeResolveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
