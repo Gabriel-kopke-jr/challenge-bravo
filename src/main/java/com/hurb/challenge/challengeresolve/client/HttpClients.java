6b3521f29c01c5918d87ba9a1089ffdaf1d7d73d
Gabriel K JR <gabrielkopke668@gmail.com>
2024-03-25 22:20:43 -0300

    feat: Create httpClient to consult an external API for currency consult

---
+++ src/main/java/com/hurb/challenge/challengeresolve/client/HttpClients.java
@@ -0,0 +1,33 @@
+package com.hurb.challenge.challengeresolve.client;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hurb.challenge.challengeresolve.model.AwesomeApi;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+@Component
+public class HttpClients {
+    private String awesomeApiUrl = "https://economia.awesomeapi.com.br/last/";
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+    private RestTemplate restTemplate = new RestTemplate();
+
+    public AwesomeApi fetchCurrency(String originalCurrency, String finalCurrency) throws JsonProcessingException {
+        String url = buildUriToSearch(originalCurrency, finalCurrency);
+        var rawData = restTemplate.getForObject(url, String.class);
+        Map<String, AwesomeApi> map = objectMapper.readValue(rawData, new TypeReference<Map<String, AwesomeApi>>() {
+        });
+        AwesomeApi awesomeApiResultFromSearch = map.values().stream().toList().get(0);
+        return awesomeApiResultFromSearch;
+    }
+
+    private String buildUriToSearch(String originalCurrency, String finalCurrency) {
+        String searchConversion = originalCurrency + "-" + finalCurrency;
+        return awesomeApiUrl + searchConversion;
+    }
+
+}
