9718a99cb7990f9fe0cf0004ec3acf213e8def52
Gabriel K JR <gabrielkopke668@gmail.com>
2024-05-24 21:15:43 -0300

    feat: returning a json to requisition

--- src/main/java/com/hurb/challenge/challengeresolve/controller/Controller.java
+++ src/main/java/com/hurb/challenge/challengeresolve/controller/Controller.java
@@ -1,5 +1,8 @@
 package com.hurb.challenge.challengeresolve.controller;
 
+import com.google.gson.JsonObject;
+import com.hurb.challenge.challengeresolve.service.MoneyConvertService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,15 +13,23 @@ import java.math.BigDecimal;
 @RestController
 @RequestMapping("api/convert")
 public class Controller {
+    @Autowired
+    private MoneyConvertService moneyConvertService;
 
     @GetMapping
-    private void convertCurrency(
+    private String convertCurrency(
         @RequestParam("from") String originCurrency,
         @RequestParam("to") String finalCurrency,
         @RequestParam("amount") BigDecimal amountMoney
         ){
 
-        System.out.println("ok sir");
+        var value = moneyConvertService.convertCurrency(originCurrency, finalCurrency, amountMoney);
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("from", originCurrency);
+        jsonObject.addProperty("to", finalCurrency);
+        jsonObject.addProperty("amount", amountMoney);
+        jsonObject.addProperty("convertedValue", value);
+        return jsonObject.toString();
 
     }
 }
