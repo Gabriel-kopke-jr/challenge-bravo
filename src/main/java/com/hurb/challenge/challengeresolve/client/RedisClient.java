0c0f7285cb0993243aa4d3637598b772cdfb2c77
Gabriel K JR <gabrielkopke668@gmail.com>
2024-07-13 15:08:15 -0300

    conf: change config for redis and add docker compose file

--- src/main/java/com/hurb/challenge/challengeresolve/client/RedisClient.java
+++ src/main/java/com/hurb/challenge/challengeresolve/client/RedisClient.java
@@ -1,13 +1,19 @@
 package com.hurb.challenge.challengeresolve.client;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import redis.clients.jedis.Jedis;
 
 @Configuration
 public class RedisClient {
+    @Value("${redis.host}")
+    private String host;
+    @Value("${redis.port}")
+    private int port;
+
     @Bean
     public Jedis redisConfig() {
-        return new Jedis("localhost", 6379);
+        return new Jedis(host, port);
     }
 }
